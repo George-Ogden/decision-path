9fdcab49005782b59d9d30104969e727dc0f584d
George Ogden <g.ogden@instadeep.com>
2023-07-11 12:14:48 +0200

    change directory structure

--- train/train.sh
+++
@@ -1,5 +0,0 @@
-torchrun --nproc_per_node=8 train.py --model resnet34 --batch-size 128 --lr 0.5 \
---lr-scheduler cosineannealinglr --lr-warmup-epochs 5 --lr-warmup-method linear \
---auto-augment ta_wide --epochs 600 --random-erase 0.1 --weight-decay 0.00002 \
---norm-weight-decay 0.0 --label-smoothing 0.1 --mixup-alpha 0.2 --cutmix-alpha 1.0 \
---train-crop-size 176 --model-ema --val-resize-size 232 --ra-sampler --ra-reps 4

2399942f4ffc464fa96a45ce8127c6281134b7e8
George-Ogden <george-ogden@outlook.com>
2023-07-23 12:07:43 +0200

    fix outliers metric

--- src/metrics/outliers.py
+++ src/metrics/outliers.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-from typing import Dict
+from typing import Dict, List
 
 from .base import Metric, VariableLengthClassifierOutput
 
@@ -10,11 +10,19 @@ class Outliers(Metric):
     def __init__(self, threshold: float = 6.):
         self.threshold = threshold
         super().__init__()
+        self.value = None
 
     def update(self, batch: Dict[str, torch.Tensor], model_output: VariableLengthClassifierOutput):
         # L x [B, N, H]
-        self.value += np.array([
-                (activations > self.threshold).float().cpu().numpy().mean(axis=(-1, -2)).sum()
-                for activations in model_output.layer_activations
-        ])
+        if self.value is None:
+            self.value = [np.zeros(activation.shape[-1], dtype=bool) for activation in model_output.layer_activations]
+        self.value = [
+            self.value[i] | (activation > self.threshold).cpu().numpy().any(axis=(-3, -2))
+            for i, activation in enumerate(model_output.layer_activations)
+        ]
         super().update(batch, model_output)
+    
+    def compute(self) -> List[float]:
+        return np.array([
+            np.mean(value) for value in self.value
+        ])

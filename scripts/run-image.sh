ca86fe8aa36b2ae870b8d0ebcd813113c27828b4
George-Ogden <george-ogden@outlook.com>
2023-07-23 19:25:51 +0200

    allow parameter specification for images

--- scripts/run-image.sh
+++ scripts/run-image.sh
@@ -1,5 +1,5 @@
-python experiment.py -m resnet18 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet
-python experiment.py -m resnet34 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet
-python experiment.py -m resnet50 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet
-python experiment.py -m resnet101 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet
-python experiment.py -m resnet152 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet
+python experiment.py -m resnet18 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet $@
+python experiment.py -m resnet34 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet $@
+python experiment.py -m resnet50 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet $@
+python experiment.py -m resnet101 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet $@
+python experiment.py -m resnet152 -me outliers kurtosis rotated-kurtosis top1 top5 rms -d imagenet $@

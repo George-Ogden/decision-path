7b7e449e8e818b5e13566e21f86f9b6abc134c6b
George Ogden <george-ogden@outlook.com>
2023-08-06 08:42:44 +0200

    remove outliers plot from small models
    doesn't make sense with RMSNorm

--- scripts/run-text.sh
+++ scripts/run-text.sh
@@ -6,5 +6,7 @@ python experiment.py -m George-Ogden/roberta-large-cased-finetuned-mnli -me outl
 python experiment.py -m George-Ogden/roberta-base-cased-finetuned-mnli -me outliers kurtosis rotated-kurtosis rms accuracy -d mnli mnli-mm -t 4. $@
 python experiment.py -m George-Ogden/gpt2-finetuned-mnli -me outliers kurtosis rotated-kurtosis rms accuracy -d mnli mnli-mm -t 10. $@
 python experiment.py -m George-Ogden/gpt2-medium-finetuned-mnli -me outliers kurtosis rotated-kurtosis rms accuracy -d mnli mnli-mm -t 10. $@
-python experiment.py -m George-Ogden/gptr2-nano-without-momentum -me outliers kurtosis rotated-kurtosis rms -d mnli mnli-mm -t 6. $@
-python experiment.py -m George-Ogden/gptr2-nano-with-momentum -me outliers kurtosis rotated-kurtosis rms -d mnli mnli-mm -t 6. $@
+python experiment.py -m George-Ogden/gptr2-nano-without-momentum-with-weight-decay -me kurtosis rotated-kurtosis rms -d mnli mnli-mm $@
+python experiment.py -m George-Ogden/gptr2-nano-with-momentum-with-weight-decay -me kurtosis rotated-kurtosis rms -d mnli mnli-mm $@
+python experiment.py -m George-Ogden/gptr2-nano-without-momentum-without-weight-decay -me kurtosis rotated-kurtosis rms -d mnli mnli-mm $@
+python experiment.py -m George-Ogden/gptr2-nano-with-momentum-without-weight-decay -me kurtosis rotated-kurtosis rms -d mnli mnli-mm $@

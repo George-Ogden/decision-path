7718bc5b517a0414c87b08c51cfa5e2f967f98ac
George Ogden <george-ogden@outlook.com>
2023-11-24 11:54:40 +0000

    define path metric

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .kurtosis import Kurtosis, RotatedKurtosis
 from .outliers import Outliers
 from .base import Metric
+from .path import Path
 from .rms import RMS
 
 METRICS = Metric.registry
---
+++ src/metrics/path.py
@@ -0,0 +1,31 @@
+import numpy as np
+import einops
+import torch
+
+from typing import Dict, List
+
+from .base import Metric, VariableLengthClassifierOutput
+
+@Metric.register("path")
+class Path(Metric):
+    def __init__(self):
+        super().__init__()
+        self.values = []
+    
+    def update(self, batch: Dict[str, torch.Tensor], model_output: VariableLengthClassifierOutput):
+        super().update(batch, model_output)
+        # L x [B, N, H]
+        self.values.append(
+            [
+                activations[:,-1,:].detach().cpu().numpy()
+                for activations in model_output.layer_activations
+            ]
+        )
+        # NB x L x [B, H]
+    
+    def compute(self) -> List[List[float]]:
+        return np.stack([
+            # [NB, B, H] -> [N, H]
+            np.concatenate(layer, axis=0)
+            for layer in zip(*self.values)
+        ]).T
